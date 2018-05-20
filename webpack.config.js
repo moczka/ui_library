7f96dc78d5fb7f035415316bc582bfaa692a0ed6
Javier Guerrero <jguerrero@wayfair.com>
2018-05-19 23:21:36 -0400

    Add resolve alias to component librabry folder

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,7 @@ const browserConfig = {
             _routes: path.resolve(__dirname, 'src/client/js/react/routes'),
             _constants: path.resolve(__dirname, 'src/client/js/constants'),
             _thunks: path.resolve(__dirname, 'src/client/js/redux/thunks'),
+            _library: path.resolve(__dirname, 'src/client/js/react/library'),
             _selectors: path.resolve(__dirname, 'src/client/js/redux/selectors'),
             _store: path.resolve(__dirname, 'src/client/js/redux/store'),
             _data: path.resolve(__dirname, 'src/data'),
